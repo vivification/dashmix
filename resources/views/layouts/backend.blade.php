fecc6504820942fa2f74877d031022e19e5d0f76
Stephen <stephen.anderson858@gmail.com>
2019-07-30 15:43:28 +0500

    Fix jquery issue.

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -31,8 +31,10 @@
 
         {{--Quotes--}}
         {{--https://laraveldaily.teachable.com/courses/378499/lectures/5842594--}}
-        <script src="{{ URL('//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js') }}"></script>
-        <script src="{{ URL('//code.jquery.com/jquery-1.11.1.min.js') }}"></script>
+
+{{--        No need to add to include these, the template already has them --}}
+{{--        <script src="{{ URL('//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js') }}"></script>--}}
+{{--        <script src="{{ URL('//code.jquery.com/jquery-1.11.1.min.js') }}"></script>--}}
 
     </head>
     <body>
